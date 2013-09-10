b54c76f80066dda3fd42e4dae233a15cb9a05d4c
combizs <combiz.s@gmail.com>
2013-09-10 00:47:44 -0700

    Remove default Jasmine Tests
    Create custom tests for Angular and added 2 scripts in 'specRunner.html' to load Angular code

---
+++ jasmine/src/index.js
@@ -0,0 +1,22 @@
+var myApp = angular.module('myApp', []);
+myApp.factory('Data', function () {
+  return { message : "I'm from the future!" }
+})
+
+myApp.filter('reverse', function() {
+  return function() {
+    return "";
+  }
+})
+
+function FirstCtrl($scope, Data) {
+  $scope.data = Data;
+}
+
+function SecondCtrl($scope, Data) {
+  $scope.data = Data;
+
+  $scope.reverseString = function (message) {
+    return message.split("").reverse().join("");
+  }
+}
