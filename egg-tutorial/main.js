ba6574d87a981a4b29e0e0227423849e83817c10
combizs <combiz.s@gmail.com>
2013-09-12 16:38:12 -0700

    Added transclude to incorporate existing html in zippy
    Used ng-click to toggle the content

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -3,6 +3,17 @@ var myApp = angular.module('myApp', []);
 myApp.directive("zippy", function () {
   return {
     restrict: "E",
-    template: "<div>Hello World!</div>"
+    transclude: true,
+    scope: {
+      title: "@"
+    },
+    template: '<div><h3 ng-click="toggleContent()">{{title}}</h3><div ng-show="isContentVisible" ng-transclude></div></div>',
+    link: function (scope) {
+      scope.isContentVisible = false;
+
+      scope.toggleContent = function () {
+        scope.isContentVisible = !scope.isContentVisible;
+      }
+    }
   }
 });
