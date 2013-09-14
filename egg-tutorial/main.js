6eb2192ada3f93e4f297294c9444cf5f612888f6
combizs <combiz.s@gmail.com>
2013-09-14 13:28:36 -0700

    Create compile function to append validElement to template element

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -1,14 +1,22 @@
 var myApp = angular.module('myApp', []);
 
 myApp.directive("dumbPassword", function () {
+
+  // create cache of element in template; similar to jQuery since we can't use .find()
+  var validElement = angular.element("<div>{{model.input}}</div>");
+
   return {
     restrict: "E",
     replace: true,
-    template: "<div><input type=\"text\" ng-model=\"model.input\"><div>{{model.input}}</div></div>",
-    link: function (scope) {
+    template: "<div><input type=\"text\" ng-model=\"model.input\"></div>",
+    compile: function (tElem) {
+      tElem.append(validElement);
+    },
+    link: function (scope, element) {
       scope.$watch("model.input", function (value) {
         if (value == "password") {
           console.log("Change it!");
+          element.children(1).toggleClass("alert-box alert");
         }
       })
     }
