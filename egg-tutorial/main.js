0dda93bb31697385731d1d9b9f19c188a2ec199c
combizs <combiz.s@gmail.com>
2013-09-14 13:19:29 -0700

    Added scope.$watch to alert user to change password if the same as 'password'

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -4,6 +4,13 @@ myApp.directive("dumbPassword", function () {
   return {
     restrict: "E",
     replace: true,
-    template: "<div><input type=\"text\" ng-model=\"model.input\"><div>{{model.input}}</div></div>"
+    template: "<div><input type=\"text\" ng-model=\"model.input\"><div>{{model.input}}</div></div>",
+    link: function (scope) {
+      scope.$watch("model.input", function (value) {
+        if (value == "password") {
+          console.log("Change it!");
+        }
+      })
+    }
   }
 });
