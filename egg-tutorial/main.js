63adf20f80f77d5ec0937ea91bd3d55e2bc462e3
combizs <combiz.s@gmail.com>
2013-09-14 13:36:28 -0700

    Clean code by creating variable 'link' and storing the function there; return 'link' in compile function

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -5,20 +5,23 @@ myApp.directive("dumbPassword", function () {
   // create cache of element in template; similar to jQuery since we can't use .find()
   var validElement = angular.element("<div>{{model.input}}</div>");
 
+  // dont use this.link; it will go to window
+  var link = function (scope) {
+    scope.$watch("model.input", function (value) {
+      if (value == "password") {
+        console.log("Change it!");
+        validElement.toggleClass("alert-box alert");
+      }
+    })
+  }
+
   return {
     restrict: "E",
     replace: true,
     template: "<div><input type=\"text\" ng-model=\"model.input\"></div>",
     compile: function (tElem) {
       tElem.append(validElement);
-      return function (scope, element) {
-        scope.$watch("model.input", function (value) {
-          if (value == "password") {
-            console.log("Change it!");
-            element.children(1).toggleClass("alert-box alert");
-          }
-        })
-      }
+      return link
     }
   }
 });
