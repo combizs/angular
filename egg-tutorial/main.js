abdad3bad6ee20c07f4118e3ed77e0056ccc5dba
combizs <combiz.s@gmail.com>
2013-09-14 12:50:26 -0700

    Set-up template in directive for angular.element

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -1,19 +1,8 @@
 var myApp = angular.module('myApp', []);
 
-myApp.directive("zippy", function () {
+myApp.directive("dumbPassword", function () {
   return {
-    restrict: "E",
-    transclude: true,
-    scope: {
-      title: "@"
-    },
-    template: '<div><h3 ng-click="toggleContent()">{{title}}</h3><div ng-show="isContentVisible" ng-transclude></div></div>',
-    link: function (scope) {
-      scope.isContentVisible = false;
-
-      scope.toggleContent = function () {
-        scope.isContentVisible = !scope.isContentVisible;
-      }
-    }
+    retrict: "E",
+    template: "<div><input type=\"text\" ng-model=\"model.input\"><div>{{model.input}}</div></div>"
   }
 });
