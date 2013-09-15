3a5f14ca3ba9bb6cf4df17a60824d0caef83ae1f
combizs <combiz.s@gmail.com>
2013-09-15 01:44:48 -0700

    Defined multiple controllers for one-page app;  'when/otherwise'

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -2,11 +2,19 @@ var myApp = angular.module('myApp', []);
 
 myApp.config(function ($routeProvider) {
   // Creates $route to be used in controller
-  $routeProvider.when('/',
-  {
-    templateUrl: "app.html",
-    controller: "AppCtrl"
-  })
+  $routeProvider
+    .when('/',
+      {
+        templateUrl: "app.html",
+        controller: "AppCtrl"
+      })
+    .when('/pizza',
+      {
+        template: 'Yum!'
+      })
+    .otherwise({
+      template: 'Does not exist!'
+    })
 })
 
 myApp.controller("AppCtrl", function ($scope, $route) {
