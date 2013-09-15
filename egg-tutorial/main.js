080072aa6335f8957f625dba72f71197eb9199c9
combizs <combiz.s@gmail.com>
2013-09-15 01:41:35 -0700

    pre config routes in route provider so it is setup early on for Ctrl

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -1,6 +1,7 @@
 var myApp = angular.module('myApp', []);
 
 myApp.config(function ($routeProvider) {
+  // Creates $route to be used in controller
   $routeProvider.when('/',
   {
     templateUrl: "app.html",
@@ -8,7 +9,7 @@ myApp.config(function ($routeProvider) {
   })
 })
 
-myApp.controller("AppCtrl", function ($scope) {
+myApp.controller("AppCtrl", function ($scope, $route) {
   $scope.model = {
     message: "this is my message"
   }
