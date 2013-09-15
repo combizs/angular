d15259220bbfe35db1d1f1795e59581cad8f679e
combizs <combiz.s@gmail.com>
2013-09-15 01:10:22 -0700

    template finally worked with id, minus the '.html'

--- egg-tutorial/main.js
+++ egg-tutorial/main.js
@@ -1,13 +1,14 @@
 var myApp = angular.module('myApp', []);
 
 myApp.directive("zippy", function ($templateCache) {
-  console.log($templateCache.get("zippy.html"));
+  console.log($templateCache.get("zippy"));
   return {
     restrict: "E",
     transclude: true,
     scope: {
       title: "@"
     },
+    // templateUrl: $templateCache.get("zippy.html"),
     templateUrl: 'zippy.html',
     link: function (scope) {
       scope.isContentVisible = false;
